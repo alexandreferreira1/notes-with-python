7062694de47a6325a90e2990d50a9431542c4dd0
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-08-28 22:58:08 -0400

    feat(notes): add filtering options for archived, favorites and pinned

--- backend/notes/crud.py
+++ backend/notes/crud.py
@@ -1,11 +1,12 @@
 from sqlalchemy.orm import Session, joinedload
+from sqlalchemy import asc, desc, func
 from notes.models import Note
 from categories.models import Category
 from notes.schemas import NoteCreate, NoteUpdate
 from sqlalchemy.exc import SQLAlchemyError
 from uuid import UUID
 from datetime import datetime
-
+from typing import Optional
 
 
 def create_note(db: Session, note_data: NoteCreate):
@@ -38,21 +39,53 @@ def create_note(db: Session, note_data: NoteCreate):
     return new_note
 
 
-def get_notes_by_user(db: Session, user_id: UUID):
-    return (
+def get_notes(db: Session, user_id: UUID, order: str = 'desc', archived: Optional[bool] = None, is_favorite: Optional[bool] = None, prioritize_pinned: Optional[bool] = None):
+    order_field = func.coalesce(Note.updated_at, Note.created_at)
+
+    query = (
         db.query(Note)
         .join(Note.category)
-        .options(joinedload(Note.category)) # eager load da categoria
+        .options(joinedload(Note.category))  # eager load da categoria
         .filter(Category.user_id == user_id, Note.deleted_at.is_(None))
-        .all()
     )
 
+    # Filtros
+    if archived is not None:
+        query = query.filter(Note.archived.is_(archived))
+    if is_favorite is not None:
+        query = query.filter(Note.is_favorite.is_(is_favorite))
+
+    # Ordenação
+    if order == 'asc':
+        if prioritize_pinned:
+            query = query.order_by(desc(Note.pinned), asc(order_field))
+        else:
+            query = query.order_by(asc(order_field))
+    else:
+        if prioritize_pinned:
+            query = query.order_by(desc(Note.pinned), desc(order_field))
+        else:
+            query = query.order_by(desc(order_field))
+
+    return query.all()
+
+def get_notes_by_user(db: Session, user_id: UUID, order: str = "desc"):
+    return get_notes(db, user_id, order, archived=False, prioritize_pinned=True)
+
+def get_favorites_notes_user(db: Session, user_id: UUID, order: str = "desc"):
+    return get_notes(db, user_id, order, is_favorite=True)
+
+def get_archived_notes_user(db: Session, user_id: UUID, order: str = "desc"):
+    return get_notes(db, user_id, order, archived=True)
+
+
 def update_note(db: Session, note_id: int, note_data: NoteUpdate):
-    note = db.query(Note).filter(Note.id == note_id, Note.deleted_at.is_(None)).first()
-    
+    note = db.query(Note).filter(Note.id == note_id,
+                                 Note.deleted_at.is_(None)).first()
+
     if not note:
         return None
-    
+
     for field, value in note_data.dict(exclude_unset=True).items():
         setattr(note, field, value)
     note.updated_at = datetime.utcnow()  # type: ignore
@@ -66,13 +99,15 @@ def update_note(db: Session, note_id: int, note_data: NoteUpdate):
 
     return note
 
+
 def delete_note(db: Session, note_id: int):
-    note = db.query(Note).filter(Note.id == note_id, Note.deleted_at.is_(None)).first()
-    
+    note = db.query(Note).filter(Note.id == note_id,
+                                 Note.deleted_at.is_(None)).first()
+
     if not note:
         return None
-    
-    note.deleted_at = datetime.utcnow() # type: ignore
+
+    note.deleted_at = datetime.utcnow()  # type: ignore
 
     try:
         db.commit()
