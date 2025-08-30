ead94a86ff95ae4dbfb6aeea09b369b47254d5bd
Alexandre Ferreira <alexandre.x.f@hotmail.com>
2025-08-29 20:02:22 -0400

    feat(notes): add routes for get favorites and archived notes

--- backend/notes/__init__.py
+++ backend/notes/__init__.py
@@ -1,3 +1,3 @@
 from .schemas import NoteCreate, NoteUpdate, NoteOut
-from .crud import create_note, get_notes_by_user, update_note
+from .crud import create_note, get_notes_by_user, get_favorites_notes_user, get_archived_notes_user, update_note, delete_note
 from .routes import router as notes_router
--- backend/notes/routes.py
+++ backend/notes/routes.py
@@ -11,22 +11,33 @@ router = APIRouter()
 def create_note(note_data: schemas.NoteCreate, db: Session = Depends(get_db)):
     return crud.create_note(db, note_data)
 
-@router.get("/users/{user_id}/notes", response_model=List[schemas.NoteOut])
-def read_user_notes(user_id: UUID, db: Session = Depends(get_db)):
-    return crud.get_notes_by_user(db, user_id)
+
+@router.get("/user/{user_id}", response_model=List[schemas.NoteOut])
+def read_user_notes(user_id: UUID, order: str = "desc", db: Session = Depends(get_db)):
+    return crud.get_notes_by_user(db, user_id, order=order)
+
+
+@router.get('/user/{user_id}/favorites', response_model=List[schemas.NoteOut])
+def read_favorites_notes(user_id: UUID, order: str = "desc", db: Session = Depends(get_db)):
+    return crud.get_favorites_notes_user(db, user_id, order=order)
+
+
+@router.get('/user/{user_id}/archived', response_model=List[schemas.NoteOut])
+def read_archived_notes(user_id: UUID, order: str = "desc", db: Session = Depends(get_db)):
+    return crud.get_archived_notes_user(db, user_id, order=order)
+
 
 @router.patch("/{note_id}", response_model=schemas.NoteOut)
 def update_note(note_id: int, note_data: schemas.NoteUpdate, db: Session = Depends(get_db)):
     updated_note = crud.update_note(db, note_id, note_data)
-
     if not updated_note:
         raise HTTPException(status_code=404, detail="Nota não encontrada")
     return updated_note
 
+
 @router.delete("/{note_id}", response_model=schemas.NoteOut)
 def delete_note(note_id: int, db: Session = Depends(get_db)):
     deleted_note = crud.delete_note(db, note_id)
-
     if not deleted_note:
         raise HTTPException(status_code=404, detail="Nota não encontrada")
     return deleted_note
